776e8a68ed6130c5237917b6e5a0fc824477dd25
justinblaber <justin.akira.blaber@gmail.com>
2018-05-16 09:36:56 -0500

    decided to skip threading and just use event queue; still need to set up saving images, saving config, and some other stuff

--- stereo_pyspin_gui.py
+++ stereo_pyspin_gui.py
@@ -2,22 +2,13 @@
 
 """ GUI for setting up stereo cameras with PySpin library """
 
-# NOTE: It appears basically nothing in matplotlib and PySpin is
-# threadsafe (to my knowdege; at the time this was written). For
-# now, everything not threadsafe is locked with a single global
-# lock. I know this is terrible because it basically means things
-# are running in serial (and opens possibility of dead locks), 
-# but with the GIL issues and matplotlib/PySpin issues this was
-# basically the only thing I tried that worked without seg faulting.
-# Overal streaming performance is not very good but was "good enough"
-# for my purposes.
+# NOTE: As of May-15-2018, It appears basically nothing in matplotlib and
+# PySpin is thread safe.
 
 # pylint: disable=global-statement
 
 import sys
-import time
-import threading
-from warnings import warn
+import queue
 from tkinter import messagebox
 
 import numpy as np
@@ -31,31 +22,36 @@ import stereo_pyspin
 
 # These are min/max values for BFS-U3-32S4M camera
 __FPS_MIN = 1
-__FPS_MAX = 60              # Manual says 118, but tests indicate its actually 60
+__FPS_MAX = 60              # Manual says 118, but tests indicate its 60
 __GAIN_MIN = 0              # Units are dB
 __GAIN_MAX = 47             # Units are dB
 __EXPOSURE_MIN = 6          # units must be micro seconds
 __EXPOSURE_MAX = 29999999   # units must be micro seconds
 
-# Set up streaming params
-__LOCK = threading.Lock()
-__THREAD_PRIMARY = None
-__THREAD_SECONDARY = None
+# Set up GUI params
+__QUEUE = queue.Queue()
 __STREAM_PRIMARY = False
 __STREAM_SECONDARY = False
 __IMSHOW_PRIMARY_DICT = {'imshow': None, 'imshow_size': None}
 __IMSHOW_SECONDARY_DICT = {'imshow': None, 'imshow_size': None}
 __HIST_PRIMARY_DICT = {'bar': None}
 __HIST_SECONDARY_DICT = {'bar': None}
-
-# GUI
 __GUI_DICT = None
 
 # -------------- #
 # Callbacks      #
 # -------------- #
 
-def __message_box_func_wrapper(func):
+def __queue_wrapper(func):
+    """ wraps function such that it gets inserted into the queue when called """
+
+    def __wrapped_func(*args, **kwargs):
+        """ wrapped function """
+
+        __QUEUE.put((func, args, kwargs))
+    return __wrapped_func
+
+def __message_box_wrapper(func):
     """ wraps function in try/except and pops up message box with exception """
 
     def __wrapped_func(*args, **kwargs):
@@ -67,214 +63,196 @@ def __message_box_func_wrapper(func):
             messagebox.showerror("Error", str(e))
     return __wrapped_func
 
+@__queue_wrapper
+@__message_box_wrapper
 def __find_and_init_primary(_=None):
     """ Finds and initializes primary camera """
 
     find_and_init_text = __GUI_DICT['cam_plot_primary_dict']['find_and_init_text'].text
 
     print('Finding primary camera...')
-
     stereo_pyspin.find_primary(find_and_init_text)
 
     print('Initializing primary camera...')
-
     stereo_pyspin.init_primary(find_and_init_text)
 
+@__queue_wrapper
+@__message_box_wrapper
 def __find_and_init_secondary(_=None):
     """ Finds and initializes secondary camera """
 
     find_and_init_text = __GUI_DICT['cam_plot_secondary_dict']['find_and_init_text'].text
 
     print('Finding secondary camera...')
-
     stereo_pyspin.find_secondary(find_and_init_text)
 
     print('Initializing secondary camera...')
-
     stereo_pyspin.init_secondary(find_and_init_text)
 
+@__queue_wrapper
+@__message_box_wrapper
 def __start_acquisition_primary(_=None):
     """ Starts acquisition of primary camera """
-    global __THREAD_PRIMARY, __STREAM_PRIMARY
+    global __STREAM_PRIMARY
 
-    print('Starting primary camera acquisition...')
     # Make sure it isn't already streaming
     if not __STREAM_PRIMARY:
         # Start streaming on camera
+        print('Starting primary camera acquisition...')
         stereo_pyspin.start_acquisition_primary()
 
-        # Set up thread
+        # Enable stream
         __STREAM_PRIMARY = True
-        __THREAD_PRIMARY = threading.Thread(target=__stream_image_primary)
-        __THREAD_PRIMARY.start()
 
+@__queue_wrapper
+@__message_box_wrapper
 def __start_acquisition_secondary(_=None):
     """ Starts acquisition of secondary camera """
-    global __THREAD_SECONDARY, __STREAM_SECONDARY
+    global __STREAM_SECONDARY
 
-    print('Starting secondary camera acquisition...')
     # Make sure it isn't already streaming
     if not __STREAM_SECONDARY:
         # Start streaming on camera
+        print('Starting secondary camera acquisition...')
         stereo_pyspin.start_acquisition_secondary()
 
-        # Set up thread
+        # Enable stream
         __STREAM_SECONDARY = True
-        __THREAD_SECONDARY = threading.Thread(target=__stream_image_secondary)
-        __THREAD_SECONDARY.start()
 
+@__queue_wrapper
+@__message_box_wrapper
 def __stop_acquisition_primary(_=None):
     """ Stops acquisition of primary camera """
-    global __THREAD_PRIMARY, __STREAM_PRIMARY
+    global __STREAM_PRIMARY
 
     # Make sure we're actually streaming
-    print('Stopping primary camera acquisition...')
     if __STREAM_PRIMARY:
         # Stop streaming on camera
+        print('Stopping primary camera acquisition...')
         stereo_pyspin.end_acquisition_primary()
 
-        # Set up thread
+        # End stream
         __STREAM_PRIMARY = False
-        __THREAD_PRIMARY.join()
-        __THREAD_PRIMARY = None
 
+@__queue_wrapper
+@__message_box_wrapper
 def __stop_acquisition_secondary(_=None):
     """ Stops acquisition of secondary camera """
-    global __THREAD_SECONDARY, __STREAM_SECONDARY
+    global __STREAM_SECONDARY
 
     # Make sure we're actually streaming
-    print('Stopping secondary camera acquisition...')
     if __STREAM_SECONDARY:
         # Stop streaming on camera
+        print('Stopping secondary camera acquisition...')
         stereo_pyspin.end_acquisition_secondary()
 
-        # Set up thread
+        # End stream
         __STREAM_SECONDARY = False
-        __THREAD_SECONDARY.join()
-        __THREAD_SECONDARY = None
 
+@__queue_wrapper
+@__message_box_wrapper
 def __fps_slider(_=None):
     """ FPS slider callback """
 
     fps = __GUI_DICT['fps_slider'].val
-
-    try:
-        # Set frame rate for cameras
-        stereo_pyspin.set_frame_rate(fps)
-    except Exception as e: # pylint: disable=broad-except,invalid-name
-        # Just print exception, the message box seems to mess up
-        # the matplotlib widgets
-        warn(str(e))
 
     # Update text to match slider
     __GUI_DICT['fps_text'].eventson = False
     __GUI_DICT['fps_text'].set_val(fps)
     __GUI_DICT['fps_text'].eventson = True
 
+    # Set frame rate for cameras
+    stereo_pyspin.set_frame_rate(fps)
+
+@__queue_wrapper
+@__message_box_wrapper
 def __fps_text(_=None):
     """ FPS text callback """
 
     fps = float(__GUI_DICT['fps_text'].text)
-
-    try:
-        # Set frame rate for cameras
-        stereo_pyspin.set_frame_rate(fps)
-    except Exception as e: # pylint: disable=broad-except,invalid-name
-        # Just print exception, the message box seems to mess up
-        # the matplotlib widgets
-        warn(str(e))
 
     # Update slider to match text
     __GUI_DICT['fps_slider'].eventson = False
     __GUI_DICT['fps_slider'].set_val(fps)
     __GUI_DICT['fps_slider'].eventson = True
 
+    # Set frame rate for cameras
+    stereo_pyspin.set_frame_rate(fps)
+
+@__queue_wrapper
+@__message_box_wrapper
 def __gain_slider(_=None):
     """ Gain slider callback """
 
     gain = __GUI_DICT['gain_slider'].val
-
-    try:
-        # Set gain for cameras
-        stereo_pyspin.set_gain(gain)
-    except Exception as e: # pylint: disable=broad-except,invalid-name
-        # Just print exception, the message box seems to mess up
-        # the matplotlib widgets
-        warn(str(e))
 
     # Update text to match slider
     __GUI_DICT['gain_text'].eventson = False
     __GUI_DICT['gain_text'].set_val(gain)
     __GUI_DICT['gain_text'].eventson = True
 
+    # Set gain for cameras
+    stereo_pyspin.set_gain(gain)
+
+@__queue_wrapper
+@__message_box_wrapper
 def __gain_text(_=None):
     """ gain text callback """
 
     gain = float(__GUI_DICT['gain_text'].text)
-
-    try:
-        # Set gain for cameras
-        stereo_pyspin.set_gain(gain)
-    except Exception as e: # pylint: disable=broad-except,invalid-name
-        # Just print exception, the message box seems to mess up
-        # the matplotlib widgets
-        warn(str(e))
 
     # Update slider to match text
     __GUI_DICT['gain_slider'].eventson = False
     __GUI_DICT['gain_slider'].set_val(gain)
     __GUI_DICT['gain_slider'].eventson = True
 
+    # Set gain for cameras
+    stereo_pyspin.set_gain(gain)
+
+@__queue_wrapper
+@__message_box_wrapper
 def __exposure_slider(_=None):
     """ Exposure slider callback """
 
     exposure = __GUI_DICT['exposure_slider'].val
-
-    try:
-        # Set exposure for cameras
-        stereo_pyspin.set_exposure(exposure)
-    except Exception as e: # pylint: disable=broad-except,invalid-name
-        # Just print exception, the message box seems to mess up
-        # the matplotlib widgets
-        warn(str(e))
 
     # Update text to match slider
     __GUI_DICT['exposure_text'].eventson = False
     __GUI_DICT['exposure_text'].set_val(exposure)
     __GUI_DICT['exposure_text'].eventson = True
 
+    # Set exposure for cameras
+    stereo_pyspin.set_exposure(exposure)
+
+@__queue_wrapper
+@__message_box_wrapper
 def __exposure_text(_=None):
     """ exposure text callback """
 
     exposure = float(__GUI_DICT['exposure_text'].text)
-
-    try:
-        # Set exposure for cameras
-        stereo_pyspin.set_exposure(exposure)
-    except Exception as e: # pylint: disable=broad-except,invalid-name
-        # Just print exception, the message box seems to mess up
-        # the matplotlib widgets
-        warn(str(e))
 
     # Update slider to match text
     __GUI_DICT['exposure_slider'].eventson = False
     __GUI_DICT['exposure_slider'].set_val(exposure)
     __GUI_DICT['exposure_slider'].eventson = True
 
-def __save_images(_=None):
-    """ save images callback """
-    global __THREAD_PRIMARY, __STREAM_PRIMARY
-    global __THREAD_SECONDARY, __STREAM_SECONDARY
+    # Set exposure for cameras
+    stereo_pyspin.set_exposure(exposure)
 
-    # Cache whether streams are running or not
-    stream_primary = __STREAM_PRIMARY
-    stream_secondary = __STREAM_SECONDARY
-
-    # Get name format, counter, and number of images
-    name_format = __GUI_DICT['name_format_text'].text
-    counter = int(__GUI_DICT['counter_text'].text)
-    num_images = int(__GUI_DICT['save_images_text'].text)
-
+#def __save_images(_=None):
+#    """ save images callback """
+#    global __THREAD_PRIMARY, __STREAM_PRIMARY
+#    global __THREAD_SECONDARY, __STREAM_SECONDARY
+#
+#    # Cache whether streams are running or not
+#    stream_primary = __STREAM_PRIMARY
+#    stream_secondary = __STREAM_SECONDARY
+#
+#    # Get name format, counter, and number of images
+#    name_format = __GUI_DICT['name_format_text'].text
+#    counter = int(__GUI_DICT['counter_text'].text)
+#    num_images = int(__GUI_DICT['save_images_text'].text)
+#
 #    # Stop streams
 #    __stop_acquisition_primary()
 #    __stop_acquisition_secondary()
@@ -425,15 +403,9 @@ def __stereo_gui(): # pylint: disable=too-many-locals,too-many-statements
     # Set initial values
     cam_plot_primary_dict['find_and_init_text'].set_val('primary.yaml')
     # Set callbacks
-    cam_plot_primary_dict['find_and_init_button'].on_clicked(
-        __message_box_func_wrapper(__find_and_init_primary)
-    )
-    cam_plot_primary_dict['start_acquisition_button'].on_clicked(
-        __message_box_func_wrapper(__start_acquisition_primary)
-    )
-    cam_plot_primary_dict['stop_acquisition_button'].on_clicked(
-        __message_box_func_wrapper(__stop_acquisition_primary)
-    )
+    cam_plot_primary_dict['find_and_init_button'].on_clicked(__find_and_init_primary)
+    cam_plot_primary_dict['start_acquisition_button'].on_clicked(__start_acquisition_primary)
+    cam_plot_primary_dict['stop_acquisition_button'].on_clicked(__stop_acquisition_primary)
 
     # Secondary camera plot
     cam_secondary_pos = [cam_primary_pos[0]+cam_primary_pos[2],
@@ -448,15 +420,9 @@ def __stereo_gui(): # pylint: disable=too-many-locals,too-many-statements
     # Set initial values
     cam_plot_secondary_dict['find_and_init_text'].set_val('secondary.yaml')
     # Set callbacks
-    cam_plot_secondary_dict['find_and_init_button'].on_clicked(
-        __message_box_func_wrapper(__find_and_init_secondary)
-    )
-    cam_plot_secondary_dict['start_acquisition_button'].on_clicked(
-        __message_box_func_wrapper(__start_acquisition_secondary)
-    )
-    cam_plot_secondary_dict['stop_acquisition_button'].on_clicked(
-        __message_box_func_wrapper(__stop_acquisition_secondary)
-    )
+    cam_plot_secondary_dict['find_and_init_button'].on_clicked(__find_and_init_secondary)
+    cam_plot_secondary_dict['start_acquisition_button'].on_clicked(__start_acquisition_secondary)
+    cam_plot_secondary_dict['stop_acquisition_button'].on_clicked(__stop_acquisition_secondary)
 
     # FPS
     fps_pos = [0, cam_primary_pos[1]-options_height, 1, options_height]
@@ -526,9 +492,7 @@ def __stereo_gui(): # pylint: disable=too-many-locals,too-many-statements
     save_images_button = Button(save_images_button_axes, 'Save Image(s)')
     save_images_button.label.set_fontsize(7)
     # Set callback
-    save_images_button.on_clicked(
-        __message_box_func_wrapper(__save_images)
-    )
+#    save_images_button.on_clicked(__save_images)
 
     # Set save image text
     save_images_text_pos = [save_images_button_pos[0]+save_images_button_pos[2]+padding+(0.5-2*padding)*0.1875+2*padding, # pylint: disable=line-too-long
@@ -615,57 +579,53 @@ def __plot_hist(image, hist_axes, hist_dict):
 
     return hist_dict
 
+@__queue_wrapper
+@__message_box_wrapper
 def __stream_image_primary():
     """ stream update of primary image """
     global __IMSHOW_PRIMARY_DICT, __HIST_PRIMARY_DICT
 
-    while __STREAM_PRIMARY and plt.fignum_exists(__GUI_DICT['fig'].number):
-        with __LOCK:
-            # Get image
-            image = stereo_pyspin.get_image_primary()
+    try:
+        # Get image
+        image = stereo_pyspin.get_image_primary()
 
-            # Plot image
-            __IMSHOW_PRIMARY_DICT = __plot_image(image,
-                                                 __GUI_DICT['cam_plot_primary_dict']['image_axes'],
-                                                 __IMSHOW_PRIMARY_DICT)
+        # Plot image
+        __IMSHOW_PRIMARY_DICT = __plot_image(image,
+                                             __GUI_DICT['cam_plot_primary_dict']['image_axes'],
+                                             __IMSHOW_PRIMARY_DICT)
 
-            # Plot histogram
-            __HIST_PRIMARY_DICT = __plot_hist(image,
-                                              __GUI_DICT['cam_plot_primary_dict']['hist_axes'],
-                                              __HIST_PRIMARY_DICT)
+        # Plot histogram
+        __HIST_PRIMARY_DICT = __plot_hist(image,
+                                          __GUI_DICT['cam_plot_primary_dict']['hist_axes'],
+                                          __HIST_PRIMARY_DICT)
+    except: # pylint: disable=bare-except
+        if __STREAM_PRIMARY:
+            # Only re-raise error if stream is enabled
+            raise
 
-            import datetime
-            print('primary: ' + str(datetime.datetime.now()))
-            sys.stdout.flush()
-
-        # Pause for a little bit. This seems to allow threads to switch around
-        time.sleep(sys.float_info.min)
-
+@__queue_wrapper
+@__message_box_wrapper
 def __stream_image_secondary():
     """ stream update of secondary image """
     global __IMSHOW_SECONDARY_DICT, __HIST_SECONDARY_DICT
 
-    while __STREAM_SECONDARY and plt.fignum_exists(__GUI_DICT['fig'].number):
-        with __LOCK:
-            # Get image
-            image = stereo_pyspin.get_image_secondary()
+    try:
+        # Get image
+        image = stereo_pyspin.get_image_secondary()
 
-            # Plot image
-            __IMSHOW_SECONDARY_DICT = __plot_image(image,
-                                                   __GUI_DICT['cam_plot_secondary_dict']['image_axes'],
-                                                   __IMSHOW_SECONDARY_DICT)
+        # Plot image
+        __IMSHOW_SECONDARY_DICT = __plot_image(image,
+                                               __GUI_DICT['cam_plot_secondary_dict']['image_axes'],
+                                               __IMSHOW_SECONDARY_DICT)
 
-            # Plot histogram
-            __HIST_SECONDARY_DICT = __plot_hist(image,
-                                                __GUI_DICT['cam_plot_secondary_dict']['hist_axes'],
-                                                __HIST_SECONDARY_DICT)
-
-            import datetime
-            print('secondary: ' + str(datetime.datetime.now()))
-            sys.stdout.flush()
-
-        # Pause for a little bit. This seems to allow threads to switch around
-        time.sleep(sys.float_info.min)
+        # Plot histogram
+        __HIST_SECONDARY_DICT = __plot_hist(image,
+                                            __GUI_DICT['cam_plot_secondary_dict']['hist_axes'],
+                                            __HIST_SECONDARY_DICT)
+    except: # pylint: disable=bare-except
+        if __STREAM_SECONDARY:
+            # Only re-raise error if stream is enabled
+            raise
 
 # -------------- #
 # Start gui      #
@@ -673,7 +633,7 @@ def __stream_image_secondary():
 
 def main():
     """ Main program """
-    global __THREAD_PRIMARY, __THREAD_SECONDARY
+    global __QUEUE
     global __STREAM_PRIMARY, __STREAM_SECONDARY
     global __IMSHOW_PRIMARY_DICT, __IMSHOW_SECONDARY_DICT
     global __HIST_PRIMARY_DICT, __HIST_SECONDARY_DICT
@@ -684,30 +644,34 @@ def main():
 
     # Update plot while figure exists
     while plt.fignum_exists(__GUI_DICT['fig'].number): # pylint: disable=unsubscriptable-object
-        with __LOCK:
-            try:
-                plt.pause(sys.float_info.min)
-            except: # pylint: disable=bare-except
-                if plt.fignum_exists(__GUI_DICT['fig'].number):
-                    # Only re-raise error if figure is still open
-                    raise
+        try:
+            # Update plot
+            plt.pause(sys.float_info.min)
 
-            import datetime
-            print('plot: ' + str(datetime.datetime.now()))
-            sys.stdout.flush()
+            # Handle streams
+            if __STREAM_PRIMARY:
+                __stream_image_primary()
 
-        # Pause for a little bit. This seems to allow threads to switch around
-        time.sleep(sys.float_info.min)
+            if __STREAM_SECONDARY:
+                __stream_image_secondary()
 
-    # Clean up threads
-    __stop_acquisition_primary()
-    __stop_acquisition_secondary()
+            # Handle queue
+            while not __QUEUE.empty():
+                func, args, kwargs = __QUEUE.get_nowait()
+                func(*args, **kwargs)
+        except: # pylint: disable=bare-except
+            if plt.fignum_exists(__GUI_DICT['fig'].number):
+                # Only re-raise error if figure is still open
+                raise
+
+    # Clean up
+    __QUEUE = queue.Queue()
+    __STREAM_PRIMARY = False
+    __STREAM_SECONDARY = False
     __IMSHOW_PRIMARY_DICT = {'imshow': None, 'imshow_size': None}
     __IMSHOW_SECONDARY_DICT = {'imshow': None, 'imshow_size': None}
     __HIST_PRIMARY_DICT = {'bar': None}
     __HIST_SECONDARY_DICT = {'bar': None}
-
-    # Clean up GUI
     __GUI_DICT = None
 
     print('Exiting...')
